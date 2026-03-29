ef4edbc686a13d46c77dec3de39c6531bb071a20
Razka Rabbany Nurdiyana <razkarabbany@student.telkomuniversity.ac.id>
2026-03-29 23:15:24 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400123/Program.cs
+++ TP_MODUL5_103022400123/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Razka");
+
+        DataGeneric<string> data = new DataGeneric<string>("103022400123"); // NIM kamu
+        data.PrintData();
+    }
+}
